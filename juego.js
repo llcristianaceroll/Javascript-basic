2d93f2ac996ed0f4a59604a127804d3a3876a1e8
Cristian Acero <llcristianaceroll@gmail.com>
2021-04-26 16:49:33 -0500

    objetos

--- juego.js
+++ juego.js
@@ -23,32 +23,71 @@
 
 //juego (piedra, papel)
 
-var piedra = "piedra";
-var papel = "papel";
-var tijera = "tijera";
+// var piedra = "piedra";
+// var papel = "papel";
+// var tijera = "tijera";
 
-function juego (usuario, maquina) {
+// function juego (usuario, maquina) {
 
-switch (true) {
+// switch (true) {
 
-    case (usuario===piedra && maquina===tijera):
-    console.log(" Gana usuario con piedra");
-    break;
+//     case (usuario===piedra && maquina===tijera):
+//     console.log(" Gana usuario con piedra");
+//     break;
 
-    case (usuario===papel && maquina===piedra):
-        console.log("Gana usuario con papel");
-    break;
+//     case (usuario===papel && maquina===piedra):
+//         console.log("Gana usuario con papel");
+//     break;
 
-    case (usuario===tijera && maquina===papel):
-    console.log(" Gana usuario con tijera");
-    break;
+//     case (usuario===tijera && maquina===papel):
+//     console.log(" Gana usuario con tijera");
+//     break;
 
-    case (maquina===usuario):
-        console.log("Empate");
-    break;
+//     case (maquina===usuario):
+//         console.log("Empate");
+//     break;
 
-    default:
-        console.log(" Gana maquina con " + maquina );
-}
-}
+//     default:
+//         console.log(" Gana maquina con " + maquina );
+// }
+// }
 //juego (piedra, papel)
+
+var estudiantes = ["Cristian","Andres","Pablo","Juan"];
+
+function saludarEstudiantes (estudiante) {
+    console.log(`Hola ${estudiante}`);
+}
+
+for(var i = 0; i < estudiantes.length; i++ ) {
+    saludarEstudiantes(estudiantes[i]);
+}
+
+for(var estudiante of estudiantes) {
+    saludarEstudiantes(estudiante);
+}
+///////////////////////////////////////////////////////////
+var estudiantes = ["Cristian","Andres","Pablo","Juan"];
+
+function saludarEstudiantes (estudiantes) {
+    console.log(`Hola ${estudiantes}`);
+}
+
+while (estudiantes.length > 0) {
+console.log(estudiantes);
+   var estudiante = estudiantes.shift();
+console.log(estudiante);
+   saludarEstudiantes(estudiante);
+}
+//////////////////Objetos
+
+var miAuto = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    annio: 2021,
+    detallDelAuto: function(){
+        console.log(`Auto ${this.modelo} ${this.annio}`);
+    }
+};
+
+miAuto.detallDelAuto();
